4f7fea78ac63aa688a500184e43a38196f2e61fb
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-28 22:26:28 +0700

    Replace Vue with Svelte

--- src/renderer/vite.config.js
+++ src/renderer/vite.config.js
@@ -1,12 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import * as path from "path"
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import autoExternal from "rollup-plugin-auto-external"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [svelte()],
+  // lib: {
+  //   entry: path.resolve(__dirname, "./index.ts"),
+  //   name: "main",
+  //   formats: ["cjs"],
+  // },
+  // rollupOptions: {
+  //   plugins: [
+  //     autoExternal({ packagePath: path.resolve(__dirname, "./package.json") }),
+  //   ],
+  // },
 
   server: {
     strictPort: true,
-    port: 8000
-  }
+    port: 8000,
+  },
 })
