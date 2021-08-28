14031f6806fe8b92158254802238d1acc88ceb49
Manassarn Manoonchai <manassarn@gmail.com>
2021-08-28 22:34:09 +0700

    Remove unused config

--- src/renderer/vite.config.js
+++ src/renderer/vite.config.js
@@ -1,22 +1,9 @@
-import * as path from "path"
 import { defineConfig } from "vite"
 import { svelte } from "@sveltejs/vite-plugin-svelte"
-import autoExternal from "rollup-plugin-auto-external"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  // lib: {
-  //   entry: path.resolve(__dirname, "./index.ts"),
-  //   name: "main",
-  //   formats: ["cjs"],
-  // },
-  // rollupOptions: {
-  //   plugins: [
-  //     autoExternal({ packagePath: path.resolve(__dirname, "./package.json") }),
-  //   ],
-  // },
-
   server: {
     strictPort: true,
     port: 8000,
